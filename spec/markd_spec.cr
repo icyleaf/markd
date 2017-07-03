5f33f3d7be6ba6461be3def90a9be1115f12913a
icyleaf <icyleaf.cn@gmail.com>
2017-07-03 16:36:36 +0800

    test: delete outdated test

--- spec/markd_spec.cr
+++ spec/markd_spec.cr
@@ -1,8 +1,6 @@
 require "./spec_helper"
 
 # describe Markd do
-#   # TODO: Write tests
-
 #   it "works" do
 #     false.should eq(true)
 #   end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,17 +1,17 @@
 require "spec"
 require "../src/markd"
 
-def assert_common_lexer_render(input, output, file = __FILE__, line = __LINE__)
-  it "renders #{input.inspect}", file, line do
-    context = Markd::Lexer::Context.new(input)
-    lexer = Markd::CommonLexer.new.call(context)
+# def assert_common_lexer_render(input, output, file = __FILE__, line = __LINE__)
+#   it "renders #{input.inspect}", file, line do
+#     context = Markd::Lexer::Context.new(input)
+#     lexer = Markd::CommonLexer.new.call(context)
 
-    document = Markd::Lexer::Document.new
-    context.document.each do |token|
-      token.delete("source")
-      document.push(token)
-    end
+#     document = Markd::Lexer::Document.new
+#     context.document.each do |token|
+#       token.delete("source")
+#       document.push(token)
+#     end
 
-    document.should eq(output), file, line
-  end
-end
+#     document.should eq(output), file, line
+#   end
+# end
