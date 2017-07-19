0d96685ae457907a0253993992a6b5ed8286bddc
icyleaf <icyleaf.cn@gmail.com>
2017-07-19 14:51:06 +0800

    test: add smart punctuation, Regression examples

--- spec/markd_spec.cr
+++ spec/markd_spec.cr
@@ -2,4 +2,6 @@ require "./spec_helper"
 
 describe_spec("fixtures/spec.txt")
 
-# describe_spec("fixtures/smart_punct.txt")
+describe_spec("fixtures/smart_punct.txt", smart: true)
+
+describe_spec("fixtures/regression.txt")
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,48 +1,52 @@
 require "spec"
 require "../src/markd"
 
-def describe_spec(file)
+def describe_spec(file, smart = false, render = false)
   specs = extract_spec_tests(file)
 
   skip_examples = [] of Int32
 
-  puts "Run [#{file}] examples"
-  examples_count = 0
-  section_count = 0
-  specs.each_with_index do |(section, examples), index|
-    section = "#{(index + 1).to_s.rjust(2)}. #{section} (#{examples.size})"
-    if skip_examples.includes?(index + 1)
-      puts section + " [SKIP]"
-      next
+  if render
+    puts "Run [#{file}] examples"
+    examples_count = 0
+    section_count = 0
+    specs.each_with_index do |(section, examples), index|
+      section = "#{(index + 1).to_s.rjust(2)}. #{section} (#{examples.size})"
+      if skip_examples.includes?(index + 1)
+        puts section + " [SKIP]"
+        next
+      end
+      section_count += 1
+      examples_count += examples.size
+      puts section
     end
-    section_count += 1
-    examples_count += examples.size
-    puts section
+    puts "Total #{section_count} describes and #{examples_count} examples"
   end
-  puts "Total #{section_count} describes and #{examples_count} examples"
 
   specs.each_with_index do |(section, examples), index|
     no = index + 1
     next if skip_examples.includes?(no)
-    assert_section(file, section, examples)
+    assert_section(file, section, examples, smart)
   end
 end
 
-def assert_section(file, section, examples)
+def assert_section(file, section, examples, smart)
   describe section do
     examples.each do |index, example|
-      assert_exapmle(file, section, index, example)
+      assert_exapmle(file, section, index, example, smart)
     end
   end
 end
 
-def assert_exapmle(file, section, index, example)
+def assert_exapmle(file, section, index, example, smart)
   markdown = example["markdown"].gsub("→", "\t").chomp
   html = example["html"].gsub("→", "\t")
   line = example["line"].to_i
 
+  options = Markd::Options.new
+  options.smart = true if smart
   it "- #{index}\n#{show_space(markdown)}", file, line do
-    output = Markd.to_html(markdown)
+    output = Markd.to_html(markdown, options)
     output.should eq(html), file, line
   end
 end
@@ -59,39 +63,44 @@ def extract_spec_tests(file)
   result_start = false
 
   path = File.expand_path(File.join("..", file), __FILE__)
-  File.open(path) do |f|
-    line_number = 0
-    while line = f.read_line
-      line_number += 1
-      line = line.gsub(/\r\n?/, "\n")
-      break if line.includes?("<!-- END TESTS -->")
+  begin
+    File.open(path) do |f|
+      line_number = 0
+      while line = f.read_line
 
-      if !test_start && !result_start && (match = line.match(/^\#{1,6}\s+(.*)$/))
-        current_section = match[1]
-        examples[current_section] = {} of Int32 => Hash(String, String)
-        example_count = 0
-      else
-        if !test_start && !result_start && line =~ /^`{32} example$/
-          test_start = true
-        elsif test_start && !result_start && line =~ /^\.$/
-          test_start = false
-          result_start = true
-        elsif !test_start && result_start && line =~ /^`{32}/
-          result_start = false
-          example_count += 1
-        elsif test_start && !result_start
-          examples[current_section][example_count] ||= {
-            "line" => line_number.to_s,
-            "markdown" => "",
-            "html" => ""
-          } of String => String
+        line_number += 1
+        line = line.gsub(/\r\n?/, "\n")
+        break if line.includes?("<!-- END TESTS -->")
 
-          examples[current_section][example_count]["markdown"] += line + "\n"
-        elsif !test_start && result_start
-          examples[current_section][example_count]["html"] += line + "\n"
+        if !test_start && !result_start && (match = line.match(/^\#{1,6}\s+(.*)$/))
+          current_section = match[1]
+          examples[current_section] = {} of Int32 => Hash(String, String)
+          example_count = 0
+        else
+          if !test_start && !result_start && line =~ /^`{32} example$/
+            test_start = true
+          elsif test_start && !result_start && line =~ /^\.$/
+            test_start = false
+            result_start = true
+          elsif !test_start && result_start && line =~ /^`{32}/
+            result_start = false
+            example_count += 1
+          elsif test_start && !result_start
+            examples[current_section][example_count] ||= {
+              "line" => line_number.to_s,
+              "markdown" => "",
+              "html" => ""
+            } of String => String
+
+            examples[current_section][example_count]["markdown"] += line + "\n"
+          elsif !test_start && result_start
+            examples[current_section][example_count]["html"] += line + "\n"
+          end
         end
       end
     end
+  rescue IO::EOFError
+    # do nothing
   end
 
   # Remove empty examples
