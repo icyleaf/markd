02fe3266a9e4f00dc57c70bad1c6851d3f929684
icyleaf <icyleaf.cn@gmail.com>
2017-07-19 15:15:03 +0800

    test: add memo comment

--- spec/markd_spec.cr
+++ spec/markd_spec.cr
@@ -1,7 +1,10 @@
 require "./spec_helper"
 
+# Commonmark spec exapmles
 describe_spec("fixtures/spec.txt")
 
+# Smart punctuation exapmles
 describe_spec("fixtures/smart_punct.txt", smart: true)
 
+# Regression exapmles
 describe_spec("fixtures/regression.txt")
