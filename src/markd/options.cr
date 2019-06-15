2c3d8a7a3ca761aa2476f55bf1959cca4f883b2a
Taylor Thurlow <taylorthurlow8@gmail.com>
2019-06-15 14:20:27 -0700

    Add support for google-code-prettify

--- src/markd/options.cr
+++ src/markd/options.cr
@@ -1,8 +1,16 @@
 module Markd
   struct Options
-    property time, gfm, toc, smart, source_pos, safe
+    property time, gfm, toc, smart, source_pos, safe, prettyprint
 
-    def initialize(@time = false, @gfm = false, @toc = false, @smart = false, @source_pos = false, @safe = false)
+    def initialize(
+      @time = false,
+      @gfm = false,
+      @toc = false,
+      @smart = false,
+      @source_pos = false,
+      @safe = false,
+      @prettyprint = false
+    )
     end
   end
 end
