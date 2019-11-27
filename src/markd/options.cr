b4480398366a0afd7e1ac05c541472281ee1d5e9
Johannes Müller <straightshoota@gmail.com>
2019-11-27 18:30:01 +0100

    Add Options#base_url to allow resolving relative links

--- src/markd/options.cr
+++ src/markd/options.cr
@@ -1,6 +1,13 @@
+require "uri"
+
 module Markd
   struct Options
     property time, gfm, toc, smart, source_pos, safe, prettyprint
+
+    # If `base_url` is not `nil`, it is used to resolve URLs of relative
+    # links. It act's like HTML's `<base href="base_url">` in the context
+    # of a Markdown document.
+    property base_url : URI?
 
     def initialize(
       @time = false,
@@ -9,7 +16,8 @@ module Markd
       @smart = false,
       @source_pos = false,
       @safe = false,
-      @prettyprint = false
+      @prettyprint = false,
+      @base_url = nil
     )
     end
   end
