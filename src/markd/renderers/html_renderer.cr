4fba2f376ef6f5fce7d2c49bee92a6604924a72a
Billy.Zheng <vil963@gmail.com>
2025-02-11 22:47:44 +0800

    Refactor

--- src/markd/renderers/html_renderer.cr
+++ src/markd/renderers/html_renderer.cr
@@ -314,13 +314,13 @@ module Markd
       link_svg = <<-'HEREDOC'
 <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path></svg>
 HEREDOC
-      {% if compare_versions(Crystal::VERSION, "1.2.0") < 0 %}
-        title = URI.encode(node.first_child.text)
-        @output_io << %(<a id="anchor-) << title << %(" class="anchor" href="#anchor-) << title << %(">) << link_svg << %( </a>)
-      {% else %}
-        title = URI.encode_path(node.first_child.text)
-        @output_io << %(<a id="anchor-) << title << %(" class="anchor" href="#anchor-) << title << %(">) << link_svg << %( </a>)
-      {% end %}
+      title = {% if compare_versions(Crystal::VERSION, "1.2.0") < 0 %}
+                URI.encode(node.first_child.text)
+              {% else %}
+                URI.encode_path(node.first_child.text)
+              {% end %}
+
+      @output_io << %(<a id="anchor-) << title << %(" class="anchor" href="#anchor-) << title << %(">) << link_svg << %( </a>)
       @last_output = ">"
     end
 
