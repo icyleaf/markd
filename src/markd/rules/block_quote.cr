721f8dc2910dc46f9dd22868064b3f5aebae6437
Roberto Alsina <roberto.alsina@gmail.com>
2025-05-09 16:32:28 -0300

    fixme

--- src/markd/rules/block_quote.cr
+++ src/markd/rules/block_quote.cr
@@ -7,7 +7,7 @@ module Markd::Rule
         seek(parser)
         parser.close_unmatched_blocks
         node = parser.add_child(Node::Type::BlockQuote, parser.next_nonspace)
-        if parser.gfm && (match = parser.line.match(/^> \[!(\w+)] (.*)$/))
+        if parser.gfm && (match = parser.line.match(/^> \[!(\w+)] (.*)$/))  # FIXME move to rule.cr
           # This is an alert
           node.data["alert"] = match[1]
           node.data["title"] = match[2].empty? ? match[1] : match[2].strip
